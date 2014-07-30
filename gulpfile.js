15405f19754342b4effdd1b153c6f5b464920316
Mark Dunkley <markdunkley@gmail.com>
2014-07-30 11:22:55 -0400

    cleaned out html index body and added bower

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
+
+// express server
+function startExpress() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')());
+  app.use(express.static(EXPRESS_ROOT));
+  app.listen(EXPRESS_PORT);
+}
+
+// tinylr for changes
+var lr;
+function startLivereload() {
+
+  lr = require('tiny-lr')();
+  lr.listen(LIVERELOAD_PORT);
+}
+
+// notify live reload changes
+function notifyLivereload(event) {
+  // `gulp.watch()` events provide an absolute path
+  // so we need to make it relative to the server root
+  var fileName = require('path').relative(EXPRESS_ROOT, event.path);
+
+  lr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+// Default task that will be run
+// when no parameter is provided
+// to gulp
+gulp.task('default', function () {
+
+  startExpress();
+  startLivereload();
+  gulp.watch('*.html', notifyLivereload);
+  gulp.watch('*.scss', ['styles']);
+});
+
+// Sass
+gulp.task('styles', function() {
+  return gulp.src('*.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest(''))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest(''));
+});
